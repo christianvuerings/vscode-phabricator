fdbf5e4edb6e3b51b0e6f2d0f097650b0123979e
Christian Vuerings <cvuerings@pinterest.com>
2020-01-20 09:50:09 -0800

    Fix timezones + add extra test

--- src/test/suite/log.test.ts
+++ src/test/suite/log.test.ts
@@ -8,14 +8,29 @@ suite("Log", () => {
   after(() => {
     vscode.window.showInformationMessage("All tests done!");
   });
-  test("logs the current time in the log message", () => {
-    sinon.useFakeTimers({
+  test("logs the current time in the log message: midnight", () => {
+    const clock = sinon.useFakeTimers({
       now: 1483228800000
     });
 
     assert.equal(
       log.append("A log output message"),
-      "[16:00:00] - A log output message"
+      "[00:00:00] - A log output message"
     );
+
+    clock.restore();
+  });
+
+  test("logs the current time in the log message: 1 hour, 1 minute and 1 second after midnight", () => {
+    const clock = sinon.useFakeTimers({
+      now: 1483228800000 + 3661000
+    });
+
+    assert.equal(
+      log.append("A log output message"),
+      "[01:01:01] - A log output message"
+    );
+
+    clock.restore();
   });
 });
